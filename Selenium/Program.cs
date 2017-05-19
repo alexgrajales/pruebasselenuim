faf67186d552ab66d5357fc99753add5332fde40
alexander grajales <alex-12-04@hotmail.com>
2017-05-18 21:53:35 -0500

    ejemplo selenium

--- Selenium/Program.cs
+++ Selenium/Program.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
 
 namespace Selenium
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
         {
+            //IWebDriver driver = new ChromeDriver(@"c:/users/alexander/documents/visual studio 2015/Projects/Selenium/Selenium/media/");
+            IWebDriver driver = new RemoteWebDriver(new Uri("http://127.0.0.1:4444/wd/hub"), DesiredCapabilities.Chrome());
+            //IWebDriver driver = new RemoteWebDriver(new Uri("http://127.0.0.1:4444/wd/hub"), new DesiredCapabilities("Chrome", "", new Platform(PlatformType.XP)));
+            driver.Url = "https://www.ilimitada.com.co/es/contenidos/cursosonline/login/";
+            driver.FindElement(By.Id("id_email")).SendKeys("alex-12-04@hotmail.com");
+            driver.FindElement(By.Id("id_password")).SendKeys("xxxxxxxxx");
+            driver.FindElement(By.Id("btnacceder")).Submit();
+            var listamenu = driver.FindElements(By.CssSelector("#footer #datosIlimitada .contenedorMenu a"));
+            for (int i = 0; i < listamenu.Count; i++)
+            {
+                listamenu = driver.FindElements(By.CssSelector("#footer #datosIlimitada .contenedorMenu a"));//se cargan las opciones de menu por el click
+                Console.WriteLine(listamenu[i].Text);
+                listamenu[i].Click();    //hacer click y refrescar la nueva pagina ya no se tienen cargadas las opciones del menu            
+            }            
         }
     }
 }
